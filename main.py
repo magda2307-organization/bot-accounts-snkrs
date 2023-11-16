02e351bbff1ab7661e25976b191a9f66690d68ad
magda2307 <98705492+magda2307@users.noreply.github.com>
2023-11-16 16:08:44 +0000

    my offline files commit

---
+++ main.py
@@ -0,0 +1,155 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import traceback
+
+# CONFIG dictionary for constants and configuration values
+CONFIG = {
+    'CONFIG_FILE': 'CONFIG.xlsx',
+    'LOG_FILE': 'log.txt',
+    'DEBUG_LOG_LEVEL': 'DEBUG',
+    'INFO_LOG_LEVEL': 'INFO',
+    'MAX_RETRIES': 3,
+    'DELAY_BETWEEN_RETRIES': 2,
+    'PROXY_CHECK_WEBSITE': 'https://www.example.com',
+    'PROXY_RETRY_ON_ERROR': 3,
+    'DO_PRINT_DEBUG': True,  # Set this to False if you don't want debug logs in the report
+    'SENDER_EMAIL': 'your_email@gmail.com',
+    'SENDER_PASSWORD': 'your_email_password',
+    'RECEIVER_EMAIL': 'receiver_email@gmail.com',
+    'SMTP_SERVER': 'smtp.gmail.com',
+    'SMTP_PORT': 587,
+}
+
+# Initialization Step
+def initialize():
+    config_file_path = CONFIG['CONFIG_FILE']
+
+    # Check if the configuration file exists
+    if not os.path.isfile(config_file_path):
+        print("Configuration file not found. Creating a new one...")
+
+        # Create a sample DataFrame with the required columns
+        sample_data = {
+            'Username': ['user1', 'user2'],
+            'Password': ['pass1', 'pass2'],
+            'Email': ['user1@example.com', 'user2@example.com'],
+            'Proxy': ['http://proxy1:port1', 'http://proxy2:port2']
+        }
+        sample_df = pd.DataFrame(sample_data)
+
+        # Write the sample DataFrame to the configuration file
+        with pd.ExcelWriter(config_file_path, engine='xlsxwriter') as writer:
+            sample_df.to_excel(writer, index=False)
+
+        print(f"Configuration file created: {config_file_path}")
+
+    # Check if the configuration file has the required columns
+    try:
+        df_config = pd.read_excel(config_file_path)
+        required_columns = {'Username', 'Password', 'Email', 'Proxy'}
+        if not required_columns.issubset(df_config.columns):
+            raise ValueError("Invalid column names in the configuration file.")
+    except pd.errors.EmptyDataError:
+        raise ValueError("Configuration file is empty. Please add data to the CONFIG.xlsx file.")
+    except Exception as e:
+        raise ValueError(f"Error reading configuration file: {e}")
+
+    print(f"Configuration file loaded: {config_file_path}")
+
+# Placeholder functions with retry mechanism
+def go_to_page(url, proxy=None, max_retries=CONFIG['MAX_RETRIES'], doPrintDebug=CONFIG['DO_PRINT_DEBUG']):
+    start_time = time.time()
+    for current_retry in range(max_retries):
+        try:
+            if doPrintDebug:
+                log(f"Attempting to check if proxy works: {proxy}", 'debug', CONFIG['DEBUG_LOG_LEVEL'], 'go_to_page')
+            
+            # Configure proxy
+            chrome_options = Options()
+            if proxy:
+                chrome_options.add_argument(f'--proxy-server={proxy}')
+            
+            # Initialize web driver with proxy configuration
+            driver = webdriver.Chrome(options=chrome_options)
+            
+            # Attempt to open a website to check if the proxy works
+            driver.get(CONFIG['PROXY_CHECK_WEBSITE'])
+            
+            if doPrintDebug:
+                log(f"Proxy check successful: {proxy}", 'debug', CONFIG['DEBUG_LOG_LEVEL'], 'go_to_page')
+            
+            # Continue with your Selenium implementation to navigate to the page
+            # Placeholder: Add your Selenium actions here
+            
+            end_time = time.time()
+            if doPrintDebug:
+                log(f"Time taken: {end_time - start_time:.2f} seconds", 'debug', CONFIG['DEBUG_LOG_LEVEL'], 'go_to_page')
+            
+            return True
+        except Exception as e:
+            if doPrintDebug:
+                log(f"Proxy check failed: {e}", 'debug', CONFIG['DEBUG_LOG_LEVEL'], 'go_to_page')
+                log(f"Retrying... Attempt {current_retry + 1}/{max_retries}", 'debug', CONFIG['DEBUG_LOG_LEVEL'], 'go_to_page')
+            time.sleep(CONFIG['DELAY_BETWEEN_RETRIES'])  # Add a delay between retries if needed
+    return False
+
+# Main Processing Loop
+def process_queue():
+    # Load user and proxy information
+    df_user = pd.read_excel(CONFIG['CONFIG_FILE'])
+    df_proxy = pd.read_excel(CONFIG['CONFIG_FILE'], sheet_name='Proxy')
+    df_combined = pd.concat([df_user, df_proxy], axis=1)
+    queue = df_combined.to_dict(orient='records')
+
+    # Main processing loop
+    for idx, item in enumerate(queue, start=1):
+        username = item['Username']
+        password = item['Password']
+        email = item['Email']
+        proxy = item.get('Proxy', '')  # Extract proxy from the config
+
+        # Use the proxy when navigating to the page
+        if go_to_page('https://example.com', proxy=proxy, doPrintDebug=True):
+            # Placeholder: Add your processing functions here
+            print(f"Processing item {idx}: {username}, {email}, {proxy}")
+            # Placeholder: Add more processing steps as needed
+
+    print("Processing complete.")
+
+# Function to log messages
+def log(message, log_type='info', log_level=CONFIG['INFO_LOG_LEVEL'], function_name=None, doPrintDebug=CONFIG['DO_PRINT_DEBUG']):
+    with open(CONFIG['LOG_FILE'], 'a') as log_file:
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        function_info = f" - {function_name}" if function_name else ""
+        log_file.write(f"[{timestamp}] [{log_level}] [{log_type.upper()}]{function_info} - {message}")
+        log_file.write("\n")
+        if doPrintDebug:
+            print(f"[{timestamp}] [{log_level}] [{log_type.upper()}]{function_info} - {message}")
+
+# Email function (placeholder)
+def send_email(report_filename):
+    # Placeholder: Add your email sending logic here
+    pass
+
+# Closing Step
+def close_process():
+    # Placeholder: Add any closing steps or resource cleanup here
+    pass
+
+# Main Execution Flow
+try:
+    initialize()
+    process_queue()
+    # Placeholder: Add additional steps or functions if needed
+    # Example: send_email('report.txt')
+    # Example: close_process()
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    print("Program execution complete.")
